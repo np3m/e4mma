628af9420ed9eb87fda4e6aeddbf3a3e39767b06
Andrew W. Steiner <awsteiner@mykolab.com>
2021-12-07 22:01:20 -0500

    Fixign qagiu regressions.

--- neutrino/Polarization.cpp
+++ neutrino/Polarization.cpp
@@ -361,6 +361,7 @@ double Polarization::CalculateDGamDq0(double E1, double q0) {
       
       if (iret!=0) {
     */
+    qags.set_limit(100);
     qags.tol_rel=1.0e-6;
     qags.tol_abs=1.0e-6;
     cout << "3";
@@ -642,21 +643,21 @@ double Polarization::CalculateInverseMFP(double E1) {
                       (&Polarization::dgamdq0_intl),
                       this,E1,std::placeholders::_1,estar,-1);
     qagiu.verbose=1;
-    std::cout << "o2scl integrating: " << E1 << " to infty." << std::endl;
+    //std::cout << "o2scl integrating: " << E1 << " to infty." << std::endl;
     //qagiu.err_nonconv=false;
     double val, err;
     qagiu.tol_rel=1.0e-6;
     qagiu.tol_abs=4.0e-19;
-    int iret=qagiu.integ(f,0.0,0.0,val,err);
+    int iret=qagiu.integ_err(f,0.0,0.0,val,err);
     if (iret!=0) {
       qagiu.tol_rel=1.0e-5;
       qagiu.tol_abs=4.0e-18;
-      iret=qagiu.integ(f,0.0,0.0,val,err);
+      iret=qagiu.integ_err(f,0.0,0.0,val,err);
     }
     if (iret!=0) {
       qagiu.tol_rel=1.0e-4;
       qagiu.tol_abs=4.0e-17;
-      iret=qagiu.integ(f,0.0,0.0,val,err);
+      iret=qagiu.integ_err(f,0.0,0.0,val,err);
     }
     if (iret!=0) {
       O2SCL_ERR("First qagiu integral failed in polarization.",
@@ -670,16 +671,16 @@ double Polarization::CalculateInverseMFP(double E1) {
 
     qagiu.tol_rel=1.0e-6;
     qagiu.tol_abs=4.0e-19;
-    int iret=qagiu.integ(f2,0.0,0.0,val,err);
+    iret=qagiu.integ_err(f2,0.0,0.0,val,err);
     if (iret!=0) {
       qagiu.tol_rel=1.0e-5;
       qagiu.tol_abs=4.0e-18;
-      iret=qagiu.integ(f2,0.0,0.0,val,err);
+      iret=qagiu.integ_err(f2,0.0,0.0,val,err);
     }
     if (iret!=0) {
       qagiu.tol_rel=1.0e-4;
       qagiu.tol_abs=4.0e-17;
-      iret=qagiu.integ(f2,0.0,0.0,val,err);
+      iret=qagiu.integ_err(f2,0.0,0.0,val,err);
     }
     if (iret!=0) {
       O2SCL_ERR("Second qagiu integral failed in polarization.",
