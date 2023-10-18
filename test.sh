4391e205688d292752d240d1b2daa3b3c46862f9
sroy14 <sroy14@vols.utk.edu>
2023-10-18 09:05:28 -0400

    updated dockerfilev2 to use v0.929a1_u23.04_py image

--- test.sh
+++ test.sh
@@ -1,3 +1,2 @@
-docker run -it --rm --name=eosv2 utk_eos2:latest \
-    curl https://isospin.roam.utk.edu/public_data/eos_tables/du21/fid_3_5_22.o2 --output data/fid_3_5_22.o2 && make mbmuses\
-    make mbmuses
+docker run -it --rm --name=utk_eos eosv2 \
+    make mbmuses \
